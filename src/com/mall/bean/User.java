dba371cf9604ccb24b53e785ea6945439677c45e
YuhangChueng <rr940720819@sina.com>
2017-08-08 20:44:22 +0800

    update1.2

--- src/com/mall/bean/User.java
+++ src/com/mall/bean/User.java
@@ -11,5 +11,53 @@ public class User {
 	private List<Address> address;
 	private List<Orders> orders;
 	private List<Comment> comments;
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getUser_tel() {
+		return user_tel;
+	}
+	public void setUser_tel(int user_tel) {
+		this.user_tel = user_tel;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_psw() {
+		return user_psw;
+	}
+	public void setUser_psw(String user_psw) {
+		this.user_psw = user_psw;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public List<Address> getAddress() {
+		return address;
+	}
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+	public List<Orders> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Orders> orders) {
+		this.orders = orders;
+	}
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
 	
 }
